ebb93d590b7381a2f2a0aa7b96c121291f62b5f6
sagar-aryal <sagar_yal92@outlook.com>
2022-09-10 21:44:59 +0300

    fixing error on refreshing github pages

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
   return (
     <div>
       <Provider store={store}>
-        <Router basename="/reactjs-website">
+        <Router>
           <NavBar />
           <Routes>
             <Route path="/" element={<Home />} />
